8fdada0ef5f4cdb5ef07853ef0f1a287de5bbc91
comerc <andrew.kachanov@gmail.com>
2019-12-06 17:05:30 +0300

    Added flutter_map to Homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './screens/homepage.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -7,105 +9,48 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Flutter Map Example',
+      // theme: ThemeData(
+      //   primarySwatch: mapBoxBlue,
+      // ),
+      home: Homepage(),
+      // routes: <String, WidgetBuilder>{
+      //   TapToAddPage.route: (context) => TapToAddPage(),
+      //   EsriPage.route: (context) => EsriPage(),
+      //   PolylinePage.route: (context) => PolylinePage(),
+      //   MapControllerPage.route: (context) => MapControllerPage(),
+      //   AnimatedMapControllerPage.route: (context) =>
+      //       AnimatedMapControllerPage(),
+      //   MarkerAnchorPage.route: (context) => MarkerAnchorPage(),
+      //   PluginPage.route: (context) => PluginPage(),
+      //   PluginScaleBar.route: (context) => PluginScaleBar(),
+      //   OfflineMapPage.route: (context) => OfflineMapPage(),
+      //   OfflineMBTilesMapPage.route: (context) => OfflineMBTilesMapPage(),
+      //   OnTapPage.route: (context) => OnTapPage(),
+      //   MovingMarkersPage.route: (context) => MovingMarkersPage(),
+      //   CirclePage.route: (context) => CirclePage(),
+      //   OverlayImagePage.route: (context) => OverlayImagePage(),
+      // },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
+// Generated using Material Design Palette/Theme Generator
+// http://mcg.mbitson.com/
+// https://github.com/mbitson/mcg
+// const int _bluePrimary = 0xFF395afa;
+// const MaterialColor mapBoxBlue = MaterialColor(
+//   _bluePrimary,
+//   <int, Color>{
+//     50: Color(0xFFE7EBFE),
+//     100: Color(0xFFC4CEFE),
+//     200: Color(0xFF9CADFD),
+//     300: Color(0xFF748CFC),
+//     400: Color(0xFF5773FB),
+//     500: Color(_bluePrimary),
+//     600: Color(0xFF3352F9),
+//     700: Color(0xFF2C48F9),
+//     800: Color(0xFF243FF8),
+//     900: Color(0xFF172EF6),
+//   },
+// );
---
+++ lib/screens/homepage.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong/latlong.dart';
+
+// import '../widgets/drawer.dart';
+
+class Homepage extends StatelessWidget {
+  static const String route = '/';
+
+  @override
+  Widget build(BuildContext context) {
+    var markers = <Marker>[
+      Marker(
+        width: 80.0,
+        height: 80.0,
+        point: LatLng(51.5, -0.09),
+        builder: (ctx) => Container(
+          child: FlutterLogo(
+            colors: Colors.blue,
+            key: ObjectKey(Colors.blue),
+          ),
+        ),
+      ),
+      Marker(
+        width: 80.0,
+        height: 80.0,
+        point: LatLng(53.3498, -6.2603),
+        builder: (ctx) => Container(
+          child: FlutterLogo(
+            colors: Colors.green,
+            key: ObjectKey(Colors.green),
+          ),
+        ),
+      ),
+      Marker(
+        width: 80.0,
+        height: 80.0,
+        point: LatLng(48.8566, 2.3522),
+        builder: (ctx) => Container(
+          child: FlutterLogo(
+            colors: Colors.purple,
+            key: ObjectKey(Colors.purple),
+          ),
+        ),
+      ),
+    ];
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Home')),
+      // drawer: buildDrawer(context, route),
+      body: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 8.0, bottom: 8.0),
+              child: Text('This is a map that is showing (51.5, -0.9).'),
+            ),
+            Flexible(
+              child: FlutterMap(
+                options: MapOptions(
+                  center: LatLng(53.9, 27.56667),
+                  zoom: 5.0,
+                ),
+                layers: [
+                  TileLayerOptions(
+                    urlTemplate: 'https://tilessputnik.ru/{z}/{x}/{y}.png',
+                    tileProvider: CachedNetworkTileProvider(),
+                  ),
+                  MarkerLayerOptions(markers: markers)
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
