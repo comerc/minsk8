f7bf3e70114f02428d84959537b47bbe0d8d1831
comerc <andrew.kachanov@gmail.com>
2019-12-16 22:24:35 +0300

    Added name to items

--- lib/widgets/showcase_card.dart
+++ lib/widgets/showcase_card.dart
@@ -10,30 +10,44 @@ class ShowcaseCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       color: Colors.white70,
-      child: Stack(
-        children: [
-          Center(
-            child: CircularProgressIndicator(),
-          ),
-          Center(
-            child: GestureDetector(
-              child: FadeInImage.memoryNetwork(
-                width: 250.0,
-                height: 250.0,
-                image: item['image']['url'][250],
-                fit: BoxFit.cover,
-                placeholder: kTransparentImage,
-              ),
-              onTap: () {
-                Navigator.pushNamed(
-                  context,
-                  '/image',
-                  arguments: item['image']['url'][1000],
-                );
-              },
+      child: SizedBox(
+        height: 250,
+        child: Stack(
+          children: [
+            Center(
+              child: CircularProgressIndicator(),
             ),
-          ),
-        ],
+            Center(
+              child: GestureDetector(
+                child: FadeInImage.memoryNetwork(
+                  width: 250.0,
+                  height: 250.0,
+                  image: item['image']['url'][250],
+                  fit: BoxFit.cover,
+                  placeholder: kTransparentImage,
+                ),
+                onTap: () {
+                  Navigator.pushNamed(
+                    context,
+                    '/image',
+                    arguments: item['image']['url'][1000],
+                  );
+                },
+              ),
+            ),
+            Container(
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                item['name'],
+                style: TextStyle(
+                  fontSize: 23,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
