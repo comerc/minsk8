38a4acb5e9abffaea743c0bb3a4f42711b76e931
comerc <andrew.kachanov@gmail.com>
2019-12-25 20:48:49 +0300

    Designed buildPrice

--- lib/widgets/price.dart
+++ lib/widgets/price.dart
@@ -5,19 +5,22 @@ Widget buildPrice(TuChongItem item) {
   return Tooltip(
     message: 'Price',
     child: Material(
+      color: Colors.yellow,
+      borderRadius: BorderRadius.all(kImageBorderRadius),
       child: InkWell(
+        splashColor: Colors.white,
         borderRadius: BorderRadius.all(kImageBorderRadius),
         child: Container(
           padding: EdgeInsets.symmetric(
-            vertical: 10,
+            vertical: 5,
             horizontal: 16.3,
           ),
           child: Text(
-            '\$99.99',
+            '23',
             style: TextStyle(
-              fontSize: kFontSize * 1.6,
-              color: Colors.orange,
-              fontWeight: FontWeight.w600,
+              fontSize: 23,
+              color: Colors.red,
+              fontWeight: FontWeight.bold,
             ),
           ),
         ),
