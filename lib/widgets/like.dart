9cf108bf06d8a231bd6c75ffebf4fac5023fd18e
comerc <andrew.kachanov@gmail.com>
2019-12-25 15:04:08 +0300

    Added Tooltip and InkWell

--- lib/widgets/like.dart
+++ lib/widgets/like.dart
@@ -3,29 +3,44 @@ import 'package:like_button/like_button.dart';
 import 'package:minsk8/import.dart';
 
 Widget buildLike(TuChongItem item) {
-  return LikeButton(
-    size: 18.0,
-    isLiked: item.isFavorite,
-    likeCount: item.favorites,
-    countBuilder: (int count, bool isLiked, String text) {
-      final color = isLiked ? Colors.pinkAccent : Colors.grey;
-      Widget result;
-      if (count == 0) {
-        result = Text(
-          "love",
-          style: TextStyle(color: color, fontSize: kFontSize),
-        );
-      } else
-        result = Text(
-          count >= 1000 ? (count / 1000.0).toStringAsFixed(1) + "k" : text,
-          style: TextStyle(color: color, fontSize: kFontSize),
-        );
-      return result;
-    },
-    likeCountAnimationType: item.favorites < 1000
-        ? LikeCountAnimationType.part
-        : LikeCountAnimationType.none,
-    onTap: (bool isLiked) => _onTap(isLiked, item),
+  return Tooltip(
+    message: 'Like',
+    child: Material(
+      child: InkWell(
+        borderRadius: BorderRadius.all(kImageBorderRadius),
+        child: LikeButton(
+          padding: EdgeInsets.symmetric(
+            vertical: 10,
+            horizontal: 16.3,
+          ),
+          size: 18.0,
+          isLiked: item.isFavorite,
+          likeCount: item.favorites,
+          countBuilder: (int count, bool isLiked, String text) {
+            final color = isLiked ? Colors.pinkAccent : Colors.grey;
+            Widget result;
+            if (count == 0) {
+              result = Text(
+                "love",
+                style: TextStyle(color: color, fontSize: kFontSize),
+              );
+            } else
+              result = Text(
+                count >= 1000
+                    ? (count / 1000.0).toStringAsFixed(1) + "k"
+                    : text,
+                style: TextStyle(color: color, fontSize: kFontSize),
+              );
+            return result;
+          },
+          likeCountAnimationType: item.favorites < 1000
+              ? LikeCountAnimationType.part
+              : LikeCountAnimationType.none,
+          onTap: (bool isLiked) => _onTap(isLiked, item),
+        ),
+        onTap: () {},
+      ),
+    ),
   );
 }
 
