2fac2ee642ad5b8373d19a8d7f4f77ee7b4946d8
comerc <andrew.kachanov@gmail.com>
2020-01-03 13:56:39 +0300

    Added withGenericHandling

--- lib/common/helpers.dart
+++ lib/common/helpers.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
 bool get isInDebugMode {
   // Assume you're in production mode.
   bool inDebugMode = false;
@@ -8,4 +11,34 @@ bool get isInDebugMode {
   assert(inDebugMode = true);
 
   return inDebugMode;
+}
+
+/// boilerplate `result.loading` and `result.hasException` handling
+///
+/// ```dart
+/// if (result.hasException) {
+///   return Text(result.exception.toString());
+/// }
+/// if (result.loading) {
+///   return const Center(
+///     child: CircularProgressIndicator(),
+///   );
+/// }
+/// ```
+QueryBuilder withGenericHandling(QueryBuilder builder) {
+  return (result, {fetchMore, refetch}) {
+    if (result.hasException) {
+      return Text(result.exception.toString());
+    }
+    if (result.loading && result.data == null) {
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+    if (result.data == null && !result.hasException) {
+      return Text(
+          'Both data and errors are null, this is a known bug after refactoring');
+    }
+    return builder(result, fetchMore: fetchMore, refetch: refetch);
+  };
 }
