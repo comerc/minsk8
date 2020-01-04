d56ac50d52e639502ebe2daab092df54d6533428
comerc <andrew.kachanov@gmail.com>
2020-01-05 01:05:15 +0300

    Change kGraphQLItemsLimit

--- lib/const/const.dart
+++ lib/const/const.dart
@@ -5,4 +5,4 @@ const Radius kImageBorderRadius = Radius.circular(10.0);
 const double kNavigationBarHeight = 60;
 const String kStorageBucket = 'gs://minsk8-2.appspot.com';
 const String kGraphQLEndpoint = 'https://minsk8.herokuapp.com/v1/graphql';
-const int kGraphQLItemsLimit = 3; // заложен +1 для вычисления 'hasMore'
+const int kGraphQLItemsLimit = 11; // заложен +1 для вычисления 'hasMore'
