9d70f08b6494431cfe25fa949e99a74e777b3c3f
comerc <andrew.kachanov@gmail.com>
2020-01-12 23:14:00 +0300

    Add Stack

--- lib/screens/item_zoom.dart
+++ lib/screens/item_zoom.dart
@@ -42,86 +42,83 @@ class _ItemZoomScreenState extends State<ItemZoomScreen>
     return WillPopScope(
       onWillPop: _onBackPressed,
       child: Material(
-        child: Column(
+        child: Stack(
+          fit: StackFit.expand,
           children: [
-            Expanded(
-              child: LayoutBuilder(
-                builder: (BuildContext context, BoxConstraints constraints) {
-                  Size size = Size(constraints.maxWidth, constraints.maxHeight);
-                  print('$size');
-                  return ExtendedImage.network(
-                    item.images[index].getDummyUrl(item.id),
-                    fit: BoxFit.contain,
-                    loadStateChanged: (state) {
-                      if (state.extendedImageLoadState != LoadState.loading)
-                        return null;
-                      return Container(
-                        alignment: Alignment.center,
-                        color: Colors.grey.withOpacity(0.3),
-                        child: CircularProgressIndicator(
-                          strokeWidth: 2.0,
-                          valueColor: AlwaysStoppedAnimation(
-                              Theme.of(context).primaryColor),
-                        ),
-                      );
-                    },
-                    //enableLoadState: false,
-                    mode: ExtendedImageMode.gesture,
-                    initGestureConfigHandler: (state) {
-                      double initialScale = 1.0;
-                      if (state.extendedImageInfo != null &&
-                          state.extendedImageInfo.image != null) {
-                        initialScale = initScale(
-                            size: size,
-                            initialScale: initialScale,
-                            imageSize: Size(
-                                state.extendedImageInfo.image.width.toDouble(),
-                                state.extendedImageInfo.image.height
-                                    .toDouble()));
-                      }
-                      return GestureConfig(
-                        minScale: 0.9,
-                        animationMinScale: 0.7,
-                        maxScale: 4.0,
-                        animationMaxScale: 4.5,
-                        speed: 1.0,
-                        inertialSpeed: 100.0,
-                        initialScale: initialScale,
-                        inPageView: false,
-                        initialAlignment: InitialAlignment.center,
-                      );
-                    },
-                    onDoubleTap: (ExtendedImageGestureState state) {
-                      ///you can use define pointerDownPosition as you can,
-                      ///default value is double tap pointer down postion.
-                      var pointerDownPosition = state.pointerDownPosition;
-                      double begin = state.gestureDetails.totalScale;
-                      double end;
-                      //remove old
-                      _animation?.removeListener(animationListener);
-                      //stop pre
-                      _animationController.stop();
-                      //reset to use
-                      _animationController.reset();
-                      if (begin == doubleTapScales[0]) {
-                        end = doubleTapScales[1];
-                      } else {
-                        end = doubleTapScales[0];
-                      }
-                      animationListener = () {
-                        //print(_animation.value);
-                        state.handleDoubleTap(
-                            scale: _animation.value,
-                            doubleTapPosition: pointerDownPosition);
-                      };
-                      _animation = _animationController
-                          .drive(Tween<double>(begin: begin, end: end));
-                      _animation.addListener(animationListener);
-                      _animationController.forward();
-                    },
-                  );
-                },
-              ),
+            LayoutBuilder(
+              builder: (BuildContext context, BoxConstraints constraints) {
+                Size size = Size(constraints.maxWidth, constraints.maxHeight);
+                return ExtendedImage.network(
+                  item.images[index].getDummyUrl(item.id),
+                  fit: BoxFit.contain,
+                  loadStateChanged: (state) {
+                    if (state.extendedImageLoadState != LoadState.loading)
+                      return null;
+                    return Container(
+                      alignment: Alignment.center,
+                      color: Colors.grey.withOpacity(0.3),
+                      child: CircularProgressIndicator(
+                        strokeWidth: 2.0,
+                        valueColor: AlwaysStoppedAnimation(
+                            Theme.of(context).primaryColor),
+                      ),
+                    );
+                  },
+                  //enableLoadState: false,
+                  mode: ExtendedImageMode.gesture,
+                  initGestureConfigHandler: (state) {
+                    double initialScale = 1.0;
+                    if (state.extendedImageInfo != null &&
+                        state.extendedImageInfo.image != null) {
+                      initialScale = initScale(
+                          size: size,
+                          initialScale: initialScale,
+                          imageSize: Size(
+                              state.extendedImageInfo.image.width.toDouble(),
+                              state.extendedImageInfo.image.height.toDouble()));
+                    }
+                    return GestureConfig(
+                      minScale: 0.9,
+                      animationMinScale: 0.7,
+                      maxScale: 4.0,
+                      animationMaxScale: 4.5,
+                      speed: 1.0,
+                      inertialSpeed: 100.0,
+                      initialScale: initialScale,
+                      inPageView: false,
+                      initialAlignment: InitialAlignment.center,
+                    );
+                  },
+                  onDoubleTap: (ExtendedImageGestureState state) {
+                    ///you can use define pointerDownPosition as you can,
+                    ///default value is double tap pointer down postion.
+                    var pointerDownPosition = state.pointerDownPosition;
+                    double begin = state.gestureDetails.totalScale;
+                    double end;
+                    //remove old
+                    _animation?.removeListener(animationListener);
+                    //stop pre
+                    _animationController.stop();
+                    //reset to use
+                    _animationController.reset();
+                    if (begin == doubleTapScales[0]) {
+                      end = doubleTapScales[1];
+                    } else {
+                      end = doubleTapScales[0];
+                    }
+                    animationListener = () {
+                      //print(_animation.value);
+                      state.handleDoubleTap(
+                          scale: _animation.value,
+                          doubleTapPosition: pointerDownPosition);
+                    };
+                    _animation = _animationController
+                        .drive(Tween<double>(begin: begin, end: end));
+                    _animation.addListener(animationListener);
+                    _animationController.forward();
+                  },
+                );
+              },
             ),
           ],
         ),
