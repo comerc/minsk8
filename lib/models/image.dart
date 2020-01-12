24df292810b7e1e7cc3afc22c3afe1b7551d839e
comerc <andrew.kachanov@gmail.com>
2020-01-12 16:23:40 +0300

    Add clearCache

--- lib/models/image.dart
+++ lib/models/image.dart
@@ -17,8 +17,23 @@ class ImageModel {
 
   getDummyUrl(String id) {
     final urlHash = generateMd5(url + id);
-    return 'https://picsum.photos/seed/2$urlHash/${width ~/ 4}/${height ~/ 4}'; // TODO: url
+    return 'https://picsum.photos/seed/$urlHash/${width ~/ 4}/${height ~/ 4}'; // TODO: url
   }
+
+  // ImageProvider createNetworkImage() {
+  //   return ExtendedNetworkImageProvider(imageUrl);
+  // }
+
+  // ImageProvider createResizeImage() {
+  //   return ResizeImage(ExtendedNetworkImageProvider(imageUrl),
+  //       width: width ~/ 5, height: height ~/ 5);
+  // }
+
+  // for SliverListConfig.collectGarbage
+  // void clearCache() {
+  //   createNetworkImage().evict();
+  //   createResizeImage().evict();
+  // }
 
   factory ImageModel.fromJson(Map<String, dynamic> json) =>
       _$ImageModelFromJson(json);
