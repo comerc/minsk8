49e20e3b7cdca79b5c13c49ef9c60fe752bdbd8e
comerc <andrew.kachanov@gmail.com>
2020-01-17 19:52:10 +0300

    Add kDefaultMapCenter

--- lib/common/const.dart
+++ lib/common/const.dart
@@ -1,11 +1,12 @@
 // import 'package:flutter/material.dart';
 
-const double kFontSize = 12;
-// const Radius kImageBorderRadius = Radius.circular(10.0);
-const double kNavigationBarHeight = 60;
-const String kStorageBucket = 'gs://minsk8-2.appspot.com';
-const String kGraphQLEndpoint = 'https://minsk8.herokuapp.com/v1/graphql';
-const int kGraphQLItemsLimit = 11; // заложен +1 для вычисления 'hasMore'
-const double kGoldenRatio = 0.61803;
-const int kTextMaxLength = 280; // как twitter
-const double kButtonHeight = 32.0;
+const kFontSize = 12.0;
+// const kImageBorderRadius = Radius.circular(10.0);
+const kNavigationBarHeight = 60.0;
+const kStorageBucket = 'gs://minsk8-2.appspot.com';
+const kGraphQLEndpoint = 'https://minsk8.herokuapp.com/v1/graphql';
+const kGraphQLItemsLimit = 11; // заложен +1 для вычисления 'hasMore'
+const kGoldenRatio = 0.61803;
+const kTextMaxLength = 280; // как twitter
+const kButtonHeight = 32.0;
+const kDefaultMapCenter = [53.9, 27.56667];
