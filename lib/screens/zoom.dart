56258c69dc2f147d1922353a50570a4e97968fb9
comerc <andrew.kachanov@gmail.com>
2020-05-24 20:32:25 +0300

    Fix deprecated isInitialRoute

--- lib/screens/zoom.dart
+++ lib/screens/zoom.dart
@@ -225,14 +225,16 @@ class _ZoomScreenState extends State<ZoomScreen>
   _buildInitialRoute(WidgetBuilder builder) {
     final settings = RouteSettings(
       name: '/zoom',
-      isInitialRoute: true,
+      // isInitialRoute: true, // deprecated
     );
     return Platform.isIOS
-        ? CupertinoPageRoute(
+        // ? CupertinoPageRoute(
+        ? NoAnimationCupertinoPageRoute(
             builder: builder,
             settings: settings,
           )
-        : MaterialPageRoute(
+        // : MaterialPageRoute(
+        : NoAnimationMaterialPageRoute(
             builder: builder,
             settings: settings,
           );
@@ -262,6 +264,66 @@ class _ZoomScreenState extends State<ZoomScreen>
       //   return route.settings.name != '/zoom';
       // },
     );
+  }
+}
+
+bool _isFirstTransitionDuration = false;
+
+class NoAnimationCupertinoPageRoute<T> extends CupertinoPageRoute<T> {
+  NoAnimationCupertinoPageRoute({
+    @required WidgetBuilder builder,
+    RouteSettings settings,
+    bool maintainState = true,
+    bool fullscreenDialog = false,
+  }) : super(
+            builder: builder,
+            maintainState: maintainState,
+            settings: settings,
+            fullscreenDialog: fullscreenDialog);
+
+  @override
+  Widget buildTransitions(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation, Widget child) {
+    return child;
+  }
+
+  @override
+  Duration get transitionDuration {
+    _isFirstTransitionDuration = !_isFirstTransitionDuration;
+    // transitionDuration вызывается два раза на каждую анимацию;
+    // для первого вызова обнуляю значение, для второго - возвращаю.
+    return _isFirstTransitionDuration
+        ? const Duration(seconds: 0)
+        : const Duration(milliseconds: 300);
+  }
+}
+
+class NoAnimationMaterialPageRoute<T> extends MaterialPageRoute<T> {
+  NoAnimationMaterialPageRoute({
+    @required WidgetBuilder builder,
+    RouteSettings settings,
+    bool maintainState = true,
+    bool fullscreenDialog = false,
+  }) : super(
+            builder: builder,
+            maintainState: maintainState,
+            settings: settings,
+            fullscreenDialog: fullscreenDialog);
+
+  @override
+  Widget buildTransitions(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation, Widget child) {
+    return child;
+  }
+
+  @override
+  Duration get transitionDuration {
+    _isFirstTransitionDuration = !_isFirstTransitionDuration;
+    // transitionDuration вызывается два раза на каждую анимацию;
+    // для первого вызова обнуляю значение, для второго - возвращаю.
+    return _isFirstTransitionDuration
+        ? const Duration(seconds: 0)
+        : const Duration(milliseconds: 300);
   }
 }
 
