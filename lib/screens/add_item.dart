736167551f64ac26f50b4559787e7be1e3f09ea0
comerc <andrew.kachanov@gmail.com>
2020-06-19 09:24:53 +0300

    Add ProgressIndicator to AddItemScreen

--- lib/screens/add_item.dart
+++ lib/screens/add_item.dart
@@ -18,7 +18,7 @@ class AddItemScreen extends StatefulWidget {
 }
 
 class _AddItemScreenState extends State<AddItemScreen> {
-  final _formKey = GlobalKey<FormState>();
+  // final _formKey = GlobalKey<FormState>();
   TextEditingController _textController;
   // bool isLoading = false;
   bool _isSubmited = false;
@@ -26,13 +26,15 @@ class _AddItemScreenState extends State<AddItemScreen> {
   ImageSource _imageSource;
   UrgentStatus _urgent = UrgentStatus.not_urgent;
   KindId _kind;
+  FocusNode _textFocusNode;
 
-  String get urgentName =>
+  String get _urgentName =>
       urgents
           .firstWhere((UrgentModel element) => element.value == _urgent,
               orElse: () => null)
           ?.name ??
       '';
+  bool get _isValidText => _textController.value.text.trim().length > 5;
 
   @override
   void initState() {
@@ -40,11 +42,13 @@ class _AddItemScreenState extends State<AddItemScreen> {
     WidgetsBinding.instance.addPostFrameCallback(_onAfterBuild);
     _textController = TextEditingController(text: '');
     _kind = widget.arguments.kind;
+    _textFocusNode = FocusNode();
   }
 
   @override
   void dispose() {
-    _textController.dispose(); // TODO: оно тут точно надо?
+    _textController.dispose();
+    _textFocusNode.dispose();
     super.dispose();
   }
 
@@ -53,91 +57,89 @@ class _AddItemScreenState extends State<AddItemScreen> {
     final size = MediaQuery.of(context).size;
     final panelChildWidth = size.width - 32.0; // for padding
     final gridSpacing = 8.0;
-    final child = Form(
-      key: _formKey,
-      child: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.only(top: 16),
-            color: Colors.white,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  height: (panelChildWidth - gridSpacing) / 2,
-                  width: panelChildWidth,
-                  child: GridView.count(
-                    crossAxisSpacing: gridSpacing,
-                    crossAxisCount: 2,
-                    children: [
-                      _buildAddImageButton(0),
-                      GridView.count(
-                        mainAxisSpacing: gridSpacing,
-                        crossAxisSpacing: gridSpacing,
-                        crossAxisCount: 2,
-                        children: List.generate(
-                          4,
-                          (i) => _buildAddImageButton(i + 1),
-                        ),
+    // final child = Form(key: _formKey, child:
+    final child = Column(
+      children: [
+        Container(
+          padding: EdgeInsets.only(top: 16),
+          color: Colors.white,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: (panelChildWidth - gridSpacing) / 2,
+                width: panelChildWidth,
+                child: GridView.count(
+                  crossAxisSpacing: gridSpacing,
+                  crossAxisCount: 2,
+                  children: [
+                    _buildAddImageButton(0),
+                    GridView.count(
+                      mainAxisSpacing: gridSpacing,
+                      crossAxisSpacing: gridSpacing,
+                      crossAxisCount: 2,
+                      children: List.generate(
+                        4,
+                        (i) => _buildAddImageButton(i + 1),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.all(16),
-            color: Colors.white,
-            child: TextFormField(
-              autofocus: false,
-              enableSuggestions: false,
-              keyboardType: TextInputType.multiline,
-              maxLines: null,
-              controller: _textController,
-              validator: _validateText,
-              decoration: InputDecoration(
-                hintText: 'Часы Casio. Рабочие.',
-                border: OutlineInputBorder(),
               ),
+            ],
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(16),
+          color: Colors.white,
+          child: TextField(
+            autofocus: false,
+            enableSuggestions: false,
+            keyboardType: TextInputType.multiline,
+            maxLines: null,
+            controller: _textController,
+            focusNode: _textFocusNode,
+            decoration: InputDecoration(
+              hintText: 'Часы Casio. Рабочие.',
+              border: OutlineInputBorder(),
             ),
           ),
-          Container(
-            constraints: BoxConstraints(minHeight: 40),
-            child: SelectButton(
-              tooltip: 'Как срочно надо отдать?',
-              text: 'Срочно?',
-              rightText: urgentName,
-              onTap: _selectUrgent,
-            ),
+        ),
+        Container(
+          constraints: BoxConstraints(minHeight: 40),
+          child: SelectButton(
+            tooltip: 'Как срочно надо отдать?',
+            text: 'Срочно?',
+            rightText: _urgentName,
+            onTap: _selectUrgent,
           ),
-          Container(
-            constraints: BoxConstraints(minHeight: 40),
-            child: SelectButton(
-              tooltip: 'Категория',
-              text: kinds.firstWhere((element) => element.value == _kind).name,
-              onTap: _selectKind,
-            ),
+        ),
+        Container(
+          constraints: BoxConstraints(minHeight: 40),
+          child: SelectButton(
+            tooltip: 'Категория',
+            text: kinds.firstWhere((element) => element.value == _kind).name,
+            onTap: _selectKind,
           ),
-          Container(
-            constraints: BoxConstraints(minHeight: 40),
-            child: SelectButton(
-              tooltip: 'Местоположение',
-              text: appState['location'] ?? 'Местоположение',
-              onTap: _selectLocation,
-            ),
+        ),
+        Container(
+          constraints: BoxConstraints(minHeight: 40),
+          child: SelectButton(
+            tooltip: 'Местоположение',
+            text: appState['location'] ?? 'Местоположение',
+            onTap: _selectLocation,
           ),
-          Spacer(),
-          Container(
-            height: kBigButtonHeight,
-            width: panelChildWidth,
-            child: ReadyButton(onTap: _handleAddItem),
-          ),
-          SizedBox(
-            height: 16,
-          ),
-        ],
-      ),
+        ),
+        Spacer(),
+        Container(
+          height: kBigButtonHeight,
+          width: panelChildWidth,
+          child: ReadyButton(onTap: _handleAddItem),
+        ),
+        SizedBox(
+          height: 16,
+        ),
+      ],
     );
     // Expanding content to fit the viewport
     final body = LayoutBuilder(
@@ -175,13 +177,31 @@ class _AddItemScreenState extends State<AddItemScreen> {
     });
   }
 
-  String _validateText(String value) =>
-      (value.isEmpty) ? 'Please Enter Text' : null;
-
-  void _handleAddItem() {
+  void _handleAddItem() async {
     // if (isLoading) {
     //   return;
     // }
+    if (!_isValidText) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            content: Text('Опишите лот: что это, состояние, размер...'),
+            actions: [
+              FlatButton(
+                child: Text('ОК'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      ).then((_) {
+        _textFocusNode.requestFocus();
+      });
+      return;
+    }
     if (_images.length == 0) {
       showDialog(
         context: context,
@@ -201,9 +221,29 @@ class _AddItemScreenState extends State<AddItemScreen> {
       );
       return;
     }
-    if (!_formKey.currentState.validate()) {
-      return;
-    }
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              buildProgressIndicator(context),
+              SizedBox(width: 8),
+              Text('Загрузка...'),
+            ],
+          ),
+        );
+      },
+    );
+    await Future.delayed(const Duration(seconds: 1));
+    Navigator.of(context).popUntil(ModalRoute.withName('/showcase'));
+    return;
+
+    // if (!_formKey.currentState.validate()) {
+    //   return;
+    // }
     // setState(() {
     //   isLoading = true;
     // });
@@ -305,8 +345,7 @@ class _AddItemScreenState extends State<AddItemScreen> {
 
   Future<bool> _onWillPop() async {
     if (_isSubmited) return true;
-    if (_images.length == 0 && _textController.value.text.trim().length < 6)
-      return true;
+    if (_images.length == 0 && !_isValidText) return true;
     final result = await showCancelItemDialog(context);
     return result ?? false; // if enableDrag, result may be null
   }
