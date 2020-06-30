0cd4f38e374eb30c8dd8407982e2e823f9b9c156
comerc <andrew.kachanov@gmail.com>
2020-06-30 21:57:49 +0300

    Add TODO

--- lib/widgets/chat.dart
+++ lib/widgets/chat.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:minsk8/import.dart';
 
+// TODO: вместо велосипедирования, прикрутить реализацию чата
+// https://uikitty.net/achat-flutter-firebase-chat-template/
+// https://pub.dev/packages/firebase_in_app_messaging
+// https://pub.dev/packages/dash_chat
+// https://gist.github.com/mancj/c298c72320666a58d0682d5ba80b74b6
+// https://beltran.work/blog/building-a-messaging-app-in-flutter-part-i-project-structure/
+// https://getstream.io/chat/
+// https://pub.dev/packages/flutter_chat
+
 class Chat extends StatelessWidget {
   // Chat(this.arguments);
 
