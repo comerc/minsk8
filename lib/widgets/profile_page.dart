91070803c4081e56ff822740e8799d172faba91e
comerc <andrew.kachanov@gmail.com>
2020-07-16 23:45:10 +0300

    Add avatar to ProfilePage

--- lib/widgets/profile_page.dart
+++ lib/widgets/profile_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:extended_image/extended_image.dart';
 import 'package:loading_more_list/loading_more_list.dart';
+import 'package:provider/provider.dart';
 import 'package:minsk8/import.dart';
 
 // TODO: https://github.com/faob-dev/folding_cell
@@ -31,20 +33,29 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final profile = Provider.of<ProfileModel>(context, listen: false);
     return Column(
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        // Container(
-        //   height: 48,
-        //   width: 48,
-        //   child: ExtendedImage.network(
-        //     member.avatarUrl,
-        //     fit: BoxFit.cover,
-        //     enableLoadState: false,
-        //   ),
-        // ),
-        Text('сколько кармы'),
+        SizedBox(height: 8),
+        // TODO: InkWell, но Ink.image не умеет делать круглую картинку
+        // https://medium.com/@RayLiVerified/create-a-rounded-image-icon-with-ripple-effect-in-flutter-eb0f4a720b90
+        // https://stackoverflow.com/questions/55565865/create-circle-button-with-network-image-and-ripple-effect
+        // https://www.codegrepper.com/code-examples/dart/ink+image+clip+flutter
+        Material(
+          shape: CircleBorder(),
+          color: Colors.white,
+          child: ExtendedImage.network(
+            profile.member.avatarUrl,
+            width: 96,
+            height: 96,
+            shape: BoxShape.circle,
+            fit: BoxFit.cover,
+            enableLoadState: false,
+          ),
+        ),
         Text('nick name'),
+        Text('сколько кармы'),
         FlatButton(
           child: Text('ПОВЫСИТЬ КАРМУ'),
           onPressed: () {
@@ -67,7 +78,7 @@ class ProfilePage extends StatelessWidget {
                       color: Colors.black.withOpacity(0.8),
                       size: kButtonIconSize,
                     ),
-                    dense: true,
+                    // dense: true,
                   ),
                   onTap: () {
                     Navigator.of(context).pushNamed(entry.key);
@@ -88,5 +99,39 @@ class ProfilePage extends StatelessWidget {
         SizedBox(height: kNavigationBarHeight * 1.5 + 8),
       ],
     );
+
+    // Stack(
+    //   fit: StackFit.expand,
+    //   children: [
+    // Positioned(
+    //   top: 96,
+    //   left: 0,
+    //   right: 0,
+    //   child: Align(
+    //     alignment: Alignment.center,
+    //     child: Material(
+    //       elevation: kButtonElevation,
+    //       borderRadius: BorderRadius.all(
+    //         Radius.circular(8),
+    //       ),
+    //       child: Container(
+    //         padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+    //         decoration: BoxDecoration(
+    //           color: Colors.white,
+    //           // border: Border.all(
+    //           //   color: Colors.grey.withOpacity(0.4),
+    //           //   width: 1,
+    //           // ),
+    //           borderRadius: BorderRadius.all(
+    //             Radius.circular(8),
+    //           ),
+    //         ),
+    //         child: Text('сколько кармы'),
+    //       ),
+    //     ),
+    //   ),
+    // ),
+    //   ],
+    // );
   }
 }
