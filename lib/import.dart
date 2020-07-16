167e73cd0301b6d6dc57ddb6415efa5c78051e5a
comerc <andrew.kachanov@gmail.com>
2020-07-16 23:21:45 +0300

    Rename map_info to map/info

--- lib/import.dart
+++ lib/import.dart
@@ -82,8 +82,8 @@ export 'widgets/life_cycle_manager.dart';
 export 'widgets/main_drawer.dart';
 // export 'widgets/map/area_layer.dart'; // local import
 export 'widgets/map/current_position.dart';
+export 'widgets/map/info.dart';
 export 'widgets/map/map.dart';
-export 'widgets/map/map_info.dart';
 // export 'widgets/map/my_item_layer.dart'; // local import
 export 'widgets/map/ready_button.dart';
 // export 'widgets/map/scale_layer.dart'; // local import
