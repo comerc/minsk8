b6f4ef673e0cd37e332f912f21b5003fb9cfd5f2
comerc <andrew.kachanov@gmail.com>
2020-07-22 02:04:16 +0300

    Remove legacy

--- lib/common/fake_data.dart
+++ lib/common/fake_data.dart
@@ -1,9 +1,1 @@
-final memberWishes = [
-  'f448c320-c09d-4a1e-8f06-cd6aba256de9',
-  '1e5b5167-d86b-477e-8c2f-ba6c6f79aba9',
-  '6014b7f4-a7a6-49ea-b9b3-275b0f3867c9',
-  '4b1bf1c6-ef1d-419b-911d-d4c177467a75',
-  'fad5178d-6800-4004-bbcd-bd2999c7ff78'
-];
-
 final memberId = '92dd31ef-991b-4e56-8f12-587e5e82ce2e';
--- lib/common/helpers.dart
+++ lib/common/helpers.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-// import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:crypto/crypto.dart';
