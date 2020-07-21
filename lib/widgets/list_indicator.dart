2bdcf79a68854b728304db42e7084e4713bb0566
comerc <andrew.kachanov@gmail.com>
2020-07-21 23:29:42 +0300

    Move buildListIndicator

---
+++ lib/widgets/list_indicator.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:loading_more_list/loading_more_list.dart';
+import 'package:minsk8/import.dart';
+
+//you can use IndicatorWidget or build yourself widget
+//in this demo, we define all status.
+Widget buildListIndicator({
+  BuildContext context,
+  IndicatorStatus status,
+  LoadingMoreBase sourceList,
+}) {
+  //if your list is sliver list ,you should build sliver indicator for it
+  //isSliver=true, when use it in sliver list
+  var isSliver = true;
+  Widget result;
+  switch (status) {
+    case IndicatorStatus.none:
+      result = Container(height: 0);
+      break;
+    case IndicatorStatus.loadingMoreBusying:
+      result = Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            // margin: EdgeInsets.only(right: 5),
+            height: 15,
+            width: 15,
+            child: buildProgressIndicator(context),
+          ),
+          // Text("正在加载...不要着急")
+        ],
+      );
+      result = _buildBackground(false, result);
+      break;
+    case IndicatorStatus.fullScreenBusying:
+      result = Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            // margin: EdgeInsets.only(right: 5),
+            height: 30,
+            width: 30,
+            child: buildProgressIndicator(context),
+          ),
+          // Text("正在加载...不要着急")
+        ],
+      );
+      result = _buildBackground(true, result);
+      if (isSliver) {
+        result = SliverFillRemaining(
+          child: result,
+        );
+      } else {
+        result = CustomScrollView(
+          slivers: [
+            SliverFillRemaining(
+              child: result,
+            )
+          ],
+        );
+      }
+      break;
+    case IndicatorStatus.error:
+      result = Text(
+        // "好像出现了问题呢？",
+        'Кажется, что-то пошло не так?',
+      );
+      result = _buildBackground(false, result);
+      result = Material(
+        child: InkWell(
+          onTap: () {
+            sourceList.errorRefresh();
+          },
+          child: result,
+        ),
+      );
+      break;
+    case IndicatorStatus.fullScreenError:
+      result = Text(
+        // "好像出现了问题呢？",
+        'Кажется, что-то пошло не так?',
+      );
+      result = _buildBackground(true, result);
+      result = Material(
+        child: InkWell(
+          onTap: () {
+            sourceList.errorRefresh();
+          },
+          child: result,
+        ),
+      );
+      if (isSliver) {
+        result = SliverFillRemaining(
+          child: result,
+        );
+      } else {
+        result = CustomScrollView(
+          slivers: [
+            SliverFillRemaining(
+              child: result,
+            )
+          ],
+        );
+      }
+      break;
+    case IndicatorStatus.noMoreLoad:
+      // result = Text("没有更多的了。。不要拖了");
+      result = Container();
+      result = _buildBackground(false, result);
+      break;
+    case IndicatorStatus.empty:
+      // result = EmptyWidget("这里是空气！");
+      result = Text('Нет данных');
+      result = _buildBackground(true, result);
+      if (isSliver) {
+        result = SliverFillRemaining(
+          // SliverToBoxAdapter( // заменил
+          child: result,
+        );
+      } else {
+        result = CustomScrollView(
+          slivers: [
+            SliverFillRemaining(
+              child: result,
+            )
+          ],
+        );
+      }
+      break;
+  }
+  return result;
+}
+
+Widget _buildBackground(bool full, Widget child) {
+  return Container(
+      padding: EdgeInsets.only(bottom: 200),
+      width: double.infinity,
+      height: full ? double.infinity : kNavigationBarHeight * 2,
+      child: child,
+      color: Colors.transparent,
+      alignment: full ? Alignment.center : Alignment.topCenter);
+}
--- lib/widgets/showcase/list.dart
+++ lib/widgets/showcase/list.dart
@@ -54,7 +54,7 @@ class _ShowcaseListState extends State<ShowcaseList>
                   SliverWaterfallFlowDelegateWithFixedCrossAxisCount(
                 crossAxisCount: crossAxisCount,
                 crossAxisSpacing: 16,
-                mainAxisSpacing: 32,
+                mainAxisSpacing: 16,
                 collectGarbage: (List<int> garbages) {
                   garbages.forEach((index) {
                     final item = widget.sourceList[index];
@@ -74,7 +74,16 @@ class _ShowcaseListState extends State<ShowcaseList>
                 );
               },
               sourceList: widget.sourceList,
-              indicatorBuilder: _buildIndicator,
+              indicatorBuilder: (
+                BuildContext context,
+                IndicatorStatus status,
+              ) {
+                return buildListIndicator(
+                  context: context,
+                  status: status,
+                  sourceList: widget.sourceList,
+                );
+              },
               // TODO: https://github.com/fluttercandies/loading_more_list/issues/20
               padding: EdgeInsets.symmetric(horizontal: 16),
               lastChildLayoutType: LastChildLayoutType.foot,
@@ -83,140 +92,5 @@ class _ShowcaseListState extends State<ShowcaseList>
         ],
       ),
     );
-  }
-
-  //you can use IndicatorWidget or build yourself widget
-  //in this demo, we define all status.
-  Widget _buildIndicator(BuildContext context, IndicatorStatus status) {
-    //if your list is sliver list ,you should build sliver indicator for it
-    //isSliver=true, when use it in sliver list
-    var isSliver = true;
-    Widget result;
-    switch (status) {
-      case IndicatorStatus.none:
-        result = Container(height: 0);
-        break;
-      case IndicatorStatus.loadingMoreBusying:
-        result = Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              // margin: EdgeInsets.only(right: 5),
-              height: 15,
-              width: 15,
-              child: buildProgressIndicator(context),
-            ),
-            // Text("正在加载...不要着急")
-          ],
-        );
-        result = _buildBackground(false, result);
-        break;
-      case IndicatorStatus.fullScreenBusying:
-        result = Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              // margin: EdgeInsets.only(right: 5),
-              height: 30,
-              width: 30,
-              child: buildProgressIndicator(context),
-            ),
-            // Text("正在加载...不要着急")
-          ],
-        );
-        result = _buildBackground(true, result);
-        if (isSliver) {
-          result = SliverFillRemaining(
-            child: result,
-          );
-        } else {
-          result = CustomScrollView(
-            slivers: [
-              SliverFillRemaining(
-                child: result,
-              )
-            ],
-          );
-        }
-        break;
-      case IndicatorStatus.error:
-        result = Text(
-          // "好像出现了问题呢？",
-          'Кажется, что-то пошло не так?',
-        );
-        result = _buildBackground(false, result);
-        result = Material(
-          child: InkWell(
-            onTap: () {
-              widget.sourceList.errorRefresh();
-            },
-            child: result,
-          ),
-        );
-        break;
-      case IndicatorStatus.fullScreenError:
-        result = Text(
-          // "好像出现了问题呢？",
-          'Кажется, что-то пошло не так?',
-        );
-        result = _buildBackground(true, result);
-        result = Material(
-          child: InkWell(
-            onTap: () {
-              widget.sourceList.errorRefresh();
-            },
-            child: result,
-          ),
-        );
-        if (isSliver) {
-          result = SliverFillRemaining(
-            child: result,
-          );
-        } else {
-          result = CustomScrollView(
-            slivers: [
-              SliverFillRemaining(
-                child: result,
-              )
-            ],
-          );
-        }
-        break;
-      case IndicatorStatus.noMoreLoad:
-        // result = Text("没有更多的了。。不要拖了");
-        result = Container();
-        result = _buildBackground(false, result);
-        break;
-      case IndicatorStatus.empty:
-        // result = EmptyWidget("这里是空气！");
-        result = Text('Нет данных');
-        result = _buildBackground(true, result);
-        if (isSliver) {
-          result = SliverFillRemaining(
-            // SliverToBoxAdapter( // заменил
-            child: result,
-          );
-        } else {
-          result = CustomScrollView(
-            slivers: [
-              SliverFillRemaining(
-                child: result,
-              )
-            ],
-          );
-        }
-        break;
-    }
-    return result;
-  }
-
-  Widget _buildBackground(bool full, Widget child) {
-    return Container(
-        padding: EdgeInsets.only(bottom: 200),
-        width: double.infinity,
-        height: full ? double.infinity : kNavigationBarHeight * 2,
-        child: child,
-        color: Colors.transparent,
-        alignment: full ? Alignment.center : Alignment.topCenter);
   }
 }
