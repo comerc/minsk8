2fb6afff5aea8b408ed1894840b59ff9a455fd73
comerc <andrew.kachanov@gmail.com>
2020-07-22 13:46:17 +0300

    Format SQL

--- lib/common/_update_total_wishes.sql
+++ lib/common/_update_total_wishes.sql
@@ -1,1 +1,3 @@
-update item set (total_wishes) = (select count(*) as total from wish where item.id = wish.item_id group by item_id) from wish where item.id = wish.item_id
+update item set (total_wishes) = 
+  (select count(*) as total from wish where item.id = wish.item_id group by item_id) 
+  from wish where item.id = wish.item_id
