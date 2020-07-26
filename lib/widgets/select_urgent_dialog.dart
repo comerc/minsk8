c4164b6509fa74999b051f338aede2a825173379
comerc <andrew.kachanov@gmail.com>
2020-07-26 17:57:19 +0300

    Improve selectUrgentDialog

--- lib/widgets/select_urgent_dialog.dart
+++ lib/widgets/select_urgent_dialog.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:loading_more_list/loading_more_list.dart';
 import 'package:minsk8/import.dart';
 
 Future<UrgentStatus> selectUrgentDialog(
     BuildContext context, UrgentStatus selected) {
   return showModalBottomSheet(
     context: context,
+    backgroundColor: Colors.white,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
+    ),
     builder: (context) {
       return Container(
         child: Column(
@@ -13,29 +18,61 @@ Future<UrgentStatus> selectUrgentDialog(
           children: [
             Padding(
               padding: EdgeInsets.only(left: 16, bottom: 16, top: 32),
-              child: Text('Как срочно надо отдать?'),
-            ),
-            ...List.generate(
-              urgents.length,
-              (index) => Material(
-                child: InkWell(
-                  child: ListTile(
-                    title: Text(urgents[index].name),
-                    subtitle: Text(urgents[index].text),
-                    selected: selected == urgents[index].value,
-                    trailing: Icon((selected == urgents[index].value
-                        ? Icons.check_box
-                        : Icons.check_box_outline_blank)),
-                    dense: true,
-                  ),
-                  onLongPress: () {}, // чтобы сократить время для splashColor
-                  onTap: () {
-                    Navigator.of(context).pop(urgents[index].value);
-                  },
+              child: Text(
+                'Как срочно надо отдать?',
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black.withOpacity(0.8),
                 ),
               ),
             ),
-            SizedBox(height: 32)
+            GlowNotificationWidget(
+              ListView.separated(
+                shrinkWrap: true,
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                itemCount: urgents.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Material(
+                    color: selected == urgents[index].value
+                        ? Colors.grey.withOpacity(0.2)
+                        : Colors.white,
+                    child: InkWell(
+                      child: ListTile(
+                        title: Text(urgents[index].name),
+                        subtitle: Text(urgents[index].text),
+                        // selected: selected == urgents[index].value,
+                        trailing: selected == urgents[index].value
+                            ? Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(4),
+                                  ),
+                                ),
+                                padding: EdgeInsets.all(4),
+                                child: Icon(
+                                  Icons.check,
+                                  color: Colors.red,
+                                  size: kButtonIconSize,
+                                ),
+                              )
+                            : null,
+                        dense: true,
+                      ),
+                      onLongPress:
+                          () {}, // чтобы сократить время для splashColor
+                      onTap: () {
+                        Navigator.of(context).pop(urgents[index].value);
+                      },
+                    ),
+                  );
+                },
+                separatorBuilder: (BuildContext context, int index) {
+                  return SizedBox(height: 8);
+                },
+              ),
+            ),
+            SizedBox(height: 32),
           ],
         ),
       );
