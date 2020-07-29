032ec74580fb7c5f77de590ad6c85e41dbaa6f4f
comerc <andrew.kachanov@gmail.com>
2020-07-29 20:03:48 +0300

    Change dimension for kPaymentSteps

--- lib/common/const.dart
+++ lib/common/const.dart
@@ -42,4 +42,4 @@ const kDefaultIconSize = 24.0;
 const kMaxWantLimit = 10;
 const kMaxWantBalance = 7;
 const kPriceFontSize = 23.0;
-const kPaymentSteps = [6, 36, 72, 144];
+const kPaymentSteps = [5, 25, 50, 100];
