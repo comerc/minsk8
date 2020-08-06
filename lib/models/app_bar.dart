aea727a6f256fd58d3579f3db0704a9ab7832c58
comerc <andrew.kachanov@gmail.com>
2020-08-06 20:57:32 +0300

    Improve ScrollBody

--- lib/models/app_bar.dart
+++ lib/models/app_bar.dart
@@ -4,6 +4,7 @@ class AppBarModel extends ChangeNotifier {
   bool _isElevation = false;
   bool get isElevation => _isElevation;
   set isElevation(bool value) {
+    if (_isElevation == value) return;
     _isElevation = value;
     notifyListeners();
   }
--- lib/widgets/scroll_body.dart
+++ lib/widgets/scroll_body.dart
@@ -15,16 +15,20 @@ class ScrollBody extends StatefulWidget {
 }
 
 class _ScrollBodyState extends State<ScrollBody> {
-  Future _future;
   ScrollController _controller;
-  var _isElevation = false;
+  AppBarModel _appBarModel;
 
   @override
   void initState() {
     super.initState();
-    _future = _reset();
+    _appBarModel = Provider.of<AppBarModel>(context, listen: false);
     _controller = ScrollController();
     _controller.addListener(_scrollListener);
+    WidgetsBinding.instance.addPostFrameCallback(_onAfterBuild);
+  }
+
+  void _onAfterBuild(Duration timeStamp) async {
+    _appBarModel.isElevation = false;
   }
 
   @override
@@ -34,47 +38,61 @@ class _ScrollBodyState extends State<ScrollBody> {
     super.dispose();
   }
 
+  // @override
+  // Widget build(BuildContext context) {
+  //   return FutureBuilder(
+  //     // https://github.com/flutter/flutter/issues/11426#issuecomment-414047398
+  //     future: _future,
+  //     builder: (BuildContext context, AsyncSnapshot snapshot) {
+  //       if (snapshot.connectionState == ConnectionState.done) {
+  //         return LayoutBuilder(
+  //           builder:
+  //               (BuildContext context, BoxConstraints viewportConstraints) {
+  //             return SingleChildScrollView(
+  //               controller: _controller,
+  //               child: ConstrainedBox(
+  //                 constraints: BoxConstraints(
+  //                   minHeight: viewportConstraints.maxHeight,
+  //                 ),
+  //                 child: widget.withIntrinsicHeight
+  //                     ? IntrinsicHeight(
+  //                         child: widget.child,
+  //                       )
+  //                     : widget.child,
+  //               ),
+  //             );
+  //           },
+  //         );
+  //       }
+  //       return Container();
+  //     },
+  //   );
+  // }
+
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      // https://github.com/flutter/flutter/issues/11426#issuecomment-414047398
-      future: _future,
-      builder: (BuildContext context, AsyncSnapshot snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          return LayoutBuilder(
-            builder:
-                (BuildContext context, BoxConstraints viewportConstraints) {
-              return SingleChildScrollView(
-                controller: _controller,
-                child: ConstrainedBox(
-                  constraints: BoxConstraints(
-                    minHeight: viewportConstraints.maxHeight,
-                  ),
-                  child: widget.withIntrinsicHeight
-                      ? IntrinsicHeight(
-                          child: widget.child,
-                        )
-                      : widget.child,
-                ),
-              );
-            },
-          );
-        }
-        return Container();
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints viewportConstraints) {
+        return SingleChildScrollView(
+          controller: _controller,
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              minHeight: viewportConstraints.maxHeight,
+            ),
+            child: widget.withIntrinsicHeight
+                ? IntrinsicHeight(
+                    child: widget.child,
+                  )
+                : widget.child,
+          ),
+        );
       },
     );
   }
 
   void _scrollListener() {
-    if (_isElevation != (_controller.offset > 1)) {
-      _isElevation = !_isElevation;
-      final appBarModel = Provider.of<AppBarModel>(context, listen: false);
-      appBarModel.isElevation = _isElevation;
+    if (_appBarModel.isElevation != (_controller.offset > 1)) {
+      _appBarModel.isElevation = !_appBarModel.isElevation;
     }
-  }
-
-  Future<void> _reset() async {
-    final appBarModel = Provider.of<AppBarModel>(context, listen: false);
-    appBarModel.isElevation = false;
   }
 }
