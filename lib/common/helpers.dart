10badb20870b0f63935559907f75a9e5289f4aea
comerc <andrew.kachanov@gmail.com>
2020-08-08 20:36:41 +0300

    Add parseJwt()

--- lib/common/helpers.dart
+++ lib/common/helpers.dart
@@ -171,3 +171,33 @@ void launchFeedback(BuildContext context, {String subject}) async {
 double getMagicHeight(double width) {
   return (width / 2) * kGoldenRatio;
 }
+
+Map<String, dynamic> parseJwt(String token) {
+  final parts = token.split('.');
+  if (parts.length != 3) {
+    throw Exception('invalid token');
+  }
+  final payload = _decodeBase64(parts[1]);
+  final payloadMap = json.decode(payload);
+  if (payloadMap is! Map<String, dynamic>) {
+    throw Exception('invalid payload');
+  }
+  return payloadMap;
+}
+
+String _decodeBase64(String str) {
+  var output = str.replaceAll('-', '+').replaceAll('_', '/');
+  switch (output.length % 4) {
+    case 0:
+      break;
+    case 2:
+      output += '==';
+      break;
+    case 3:
+      output += '=';
+      break;
+    default:
+      throw Exception('Illegal base64url string!"');
+  }
+  return utf8.decode(base64Url.decode(output));
+}
