d6ca53aa9f4ad89b0880408224d05f252f21c36e
comerc <andrew.kachanov@gmail.com>
2020-08-12 08:08:02 +0300

    Fix iconSize

--- lib/widgets/showcase/app_bar.dart
+++ lib/widgets/showcase/app_bar.dart
@@ -34,6 +34,7 @@ class ShowcaseAppBar extends StatelessWidget {
         IconButton(
           tooltip: 'Настройки',
           icon: Icon(FontAwesomeIcons.slidersH),
+          iconSize: kButtonIconSize,
           onPressed: () {
             Navigator.of(context).pushNamed('/showcase_map');
           },
@@ -41,62 +42,5 @@ class ShowcaseAppBar extends StatelessWidget {
       ],
       elevation: 0,
     );
-    // TODO: заменить на стандартный AppBar
-    // return Container(
-    //   color: Colors.white,
-    //   child: Row(
-    //     crossAxisAlignment: CrossAxisAlignment.center,
-    //     children: <Widget>[
-    //       SizedBox(width: 16),
-    //       Column(
-    //         mainAxisAlignment: MainAxisAlignment.center,
-    //         crossAxisAlignment: CrossAxisAlignment.start,
-    //         children: <Widget>[
-    //           Container(
-    //             child: Text(
-    //               'Без названия',
-    //               style: AppBarTheme.of(context).textTheme.headline6,
-    //             ),
-    //           ),
-    //           Container(
-    //             padding: EdgeInsets.only(top: 4),
-    //             child: Text(
-    //               "${appState['ShowcaseMap.address']} — ${appState['ShowcaseMap.radius']} км",
-    //               style: TextStyle(
-    //                 fontSize: kFontSize,
-    //                 fontWeight: FontWeight.w600,
-    //                 color: Colors.black.withOpacity(0.6),
-    //               ),
-    //             ),
-    //           ),
-    //         ],
-    //       ),
-    //       Spacer(),
-    //       AspectRatio(
-    //         aspectRatio: 1,
-    //         child: Tooltip(
-    //           message: 'Настройки',
-    //           child: Material(
-    //             color: Colors.white,
-    //             child: InkWell(
-    //               borderRadius:
-    //                   BorderRadius.all(Radius.circular(kButtonIconSize * 2)),
-    //               child: Container(
-    //                 child: Icon(
-    //                   FontAwesomeIcons.slidersH,
-    //                   color: Colors.black.withOpacity(0.8),
-    //                   size: kButtonIconSize,
-    //                 ),
-    //               ),
-    //               onTap: () {
-    //                 Navigator.of(context).pushNamed('/showcase_map');
-    //               },
-    //             ),
-    //           ),
-    //         ),
-    //       ),
-    //     ],
-    //   ),
-    // );
   }
 }
