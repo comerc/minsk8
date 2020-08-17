ef2a8e1e2cc14d5ebc4da85c4267b35cd0c72a7c
comerc <andrew.kachanov@gmail.com>
2020-08-17 08:33:19 +0300

    Improve LoginScreen

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,37 +1,18 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:minsk8/import.dart';
-
-// TODO: Step-by-step guide to Android code signing and code signing https://blog.codemagic.io/the-simple-guide-to-android-code-signing/
 
 // IF (SELECT id FROM mytable WHERE other_key = 'SOMETHING' LIMIT 1) < 0 THEN
 //  INSERT INTO mytable (other_key) VALUES ('SOMETHING')
 // END IF
 
-// https://github.com/flutter/flutter/issues/33393#issuecomment-510395178
-
-// A tutorial for using Firebase to add authentication and authorization to a realtime Hasura app https://hasura.io/blog/authentication-and-authorization-using-hasura-and-firebase/
-// Hasura Authentication with Firebase https://medium.com/swlh/hasura-authentication-with-firebase-ee5543d57772
-// Super Simple Authentication Flow with Flutter & Firebase https://medium.com/coding-with-flutter/super-simple-authentication-flow-with-flutter-firebase-737bba04924c
-
-// How to add SHA1 https://stackoverflow.com/a/57505927
-
-// TODO: flutter_secure_storage для хранения токена авторизации?
 // TODO: [MVP] реализовать аутентификацию через: FB, Google, Apple Id, VK, Telegram
-// Google Sign In https://medium.com/flutter-community/flutter-implementing-google-sign-in-71888bca24ed
+// + Google Sign In https://medium.com/flutter-community/flutter-implementing-google-sign-in-71888bca24ed
 // Facebook Sign In https://medium.com/@karlwhiteprivate/flutter-facebook-sign-in-with-firebase-in-2020-66556a8c3586
 // Apple SignIn https://medium.com/@karlwhiteprivate/flutter-firebase-sign-in-with-apple-c99967df142f
-// https://ru.stackoverflow.com/questions/667654/%D0%90%D0%B2%D1%82%D0%BE%D1%80%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D1%8F-vk-%D0%B2-%D1%81%D0%B2%D1%8F%D0%B7%D0%BA%D0%B5-%D1%81-firebase
-// https://qna.habr.com/q/587426
-// TODO: как объединять аккаунты https://firebase.google.com/docs/auth/android/account-linking
-// TODO: аутентификация через одноразовый пароль в телегу
-// http://www.outsidethebox.ms/18835/
-// https://habr.com/ru/post/331502/
-// https://habr.com/ru/post/321682/
-// https://github.com/Flutterando/auth-service/
+// VK Sign In https://ru.stackoverflow.com/questions/667654/%D0%90%D0%B2%D1%82%D0%BE%D1%80%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D1%8F-vk-%D0%B2-%D1%81%D0%B2%D1%8F%D0%B7%D0%BA%D0%B5-%D1%81-firebase
 
 // Аутентификация пользователя через вк:
 // https://firebase.google.com/docs/auth/android/custom-auth
@@ -40,12 +21,19 @@ import 'package:minsk8/import.dart';
 // Либо можно на клиенте авторизовывать в вк, а в firebase передавать как авторизацию по почте,
 // придумав пароль за пользователя.
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   LoginScreen({this.onClose});
 
   final Function(AuthData authData) onClose;
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
   final GoogleSignIn _googleSignIn = GoogleSignIn();
   final _scaffoldKey = GlobalKey<ScaffoldState>();
+  var _isLoading = false;
 
   @override
   Widget build(BuildContext context) {
@@ -76,43 +64,19 @@ class LoginScreen extends StatelessWidget {
                   Text('Войти через Google'),
                 ],
               ),
-              onLongPress: () {}, // чтобы сократить время для splashColor
-              onPressed: () async {
-                try {
-                  final credential = await _signInWithGoogle();
-                  final authResult = await FirebaseAuth.instance
-                      .signInWithCredential(credential);
-                  final user = authResult.user;
-                  final token = await _getToken(context: context, user: user);
-                  Navigator.of(context).pop();
-                  onClose(AuthData(user: user, token: token));
-                } on PlatformException catch (error) {
-                  final snackBar = SnackBar(
-                    content: Text(error.message),
-                    action: SnackBarAction(
-                      label: 'Сообщить о проблеме',
-                      onPressed: () {
-                        launchFeedback(
-                          context,
-                          subject: 'Сообщить о проблеме',
-                          isAnonymous: true,
-                        );
-                      },
-                    ),
-                  );
-                  _scaffoldKey.currentState.showSnackBar(snackBar);
-                  debugPrint(error.toString());
-                }
-              },
+              onLongPress: _isLoading
+                  ? null
+                  : () {}, // чтобы сократить время для splashColor
+              onPressed: _isLoading ? null : _signInWithGoogle,
             ),
             if (isInDebugMode)
               OutlineButton(
                 shape: StadiumBorder(),
                 child: Text('Sign Out'),
-                onLongPress: () {}, // чтобы сократить время для splashColor
-                onPressed: () {
-                  _signOutWithGoogle();
-                },
+                onLongPress: _isLoading
+                    ? null
+                    : () {}, // чтобы сократить время для splashColor
+                onPressed: _isLoading ? null : _signOut,
               ),
           ],
         ),
@@ -156,23 +120,65 @@ class LoginScreen extends StatelessWidget {
     return idToken.token;
   }
 
-  Future<AuthCredential> _signInWithGoogle() async {
-    // TODO: после регистрации на Web, запускать без дополнительных вопросов (как WeBull)
-    // var googleSignInAccount = _googleSignIn.currentUser;
-    // googleSignInAccount ??=
-    //     await _googleSignIn.signInSilently(); // exception workaround: CTRL+F5
-    // googleSignInAccount ??= await _googleSignIn.signIn();
-    final googleSignInAccount = await _googleSignIn.signIn();
-    final googleSignInAuthentication = await googleSignInAccount.authentication;
-    return GoogleAuthProvider.getCredential(
-      accessToken: googleSignInAuthentication.accessToken,
-      idToken: googleSignInAuthentication.idToken,
-    );
+  void _signIn({Future<AuthCredential> Function() getCredentials}) async {
+    try {
+      setState(() {
+        _isLoading = true;
+      });
+      final credential = await getCredentials();
+      final authResult =
+          await FirebaseAuth.instance.signInWithCredential(credential);
+      final user = authResult.user;
+      final token = await _getToken(context: context, user: user);
+      Navigator.of(context).pop();
+      widget.onClose(AuthData(user: user, token: token));
+    } catch (error) {
+      setState(() {
+        _isLoading = false;
+      });
+      final snackBar = SnackBar(
+        content: Text(error.toString()),
+        action: SnackBarAction(
+          label: 'Сообщить о проблеме',
+          onPressed: () {
+            launchFeedback(
+              context,
+              subject: 'Сообщить о проблеме',
+              isAnonymous: true,
+            );
+          },
+        ),
+      );
+      _scaffoldKey.currentState.showSnackBar(snackBar);
+      debugPrint(error.toString());
+    }
   }
 
-  Future<void> _signOutWithGoogle() async {
+  void _signInWithGoogle() {
+    _signIn(getCredentials: () async {
+      // TODO: после регистрации на Web, запускать без дополнительных вопросов (как WeBull)
+      // var googleSignInAccount = _googleSignIn.currentUser;
+      // googleSignInAccount ??=
+      //     await _googleSignIn.signInSilently(); // exception workaround: CTRL+F5
+      // googleSignInAccount ??= await _googleSignIn.signIn();
+      final googleSignInAccount = await _googleSignIn.signIn();
+      final googleSignInAuthentication =
+          await googleSignInAccount.authentication;
+      return GoogleAuthProvider.getCredential(
+        accessToken: googleSignInAuthentication.accessToken,
+        idToken: googleSignInAuthentication.idToken,
+      );
+    });
+  }
+
+  void _signOut() async {
+    final user = await FirebaseAuth.instance.currentUser();
+    final idToken = await user.getIdToken();
+    final signInProvider = idToken.signInProvider;
     await FirebaseAuth.instance.signOut();
-    await _googleSignIn.disconnect();
-    await _googleSignIn.signOut();
+    if (signInProvider == 'google.com' && await _googleSignIn.isSignedIn()) {
+      await _googleSignIn.disconnect();
+      await _googleSignIn.signOut();
+    }
   }
 }
