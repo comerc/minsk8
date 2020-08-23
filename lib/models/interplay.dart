0586c5d81f9522a1cb0684bd9274ceff93cac9ed
comerc <andrew.kachanov@gmail.com>
2020-08-23 21:10:41 +0300

    Rename InteractionModel to InterplayModel

---
+++ lib/models/interplay.dart
@@ -0,0 +1,17 @@
+import 'package:minsk8/import.dart';
+
+class InterplayModel implements EnumModel {
+  InterplayModel(InterplayValue value, String name)
+      : _value = value,
+        _name = name;
+
+  final InterplayValue _value;
+  final String _name;
+
+  @override
+  InterplayValue get value => _value;
+  @override
+  String get name => _name;
+}
+
+enum InterplayValue { chat, notice }
