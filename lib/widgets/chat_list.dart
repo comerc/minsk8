810140394389e64736c55eff377bb93e15844822
comerc <andrew.kachanov@gmail.com>
2020-08-27 00:43:10 +0300

    Investigate animation for ChatListGroup

--- lib/widgets/chat_list.dart
+++ lib/widgets/chat_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart' show timeDilation;
 import 'package:intl/intl.dart';
 import 'package:extended_image/extended_image.dart';
 import 'package:loading_more_list/loading_more_list.dart';
@@ -42,114 +43,73 @@ class ChatState extends State<ChatList> with AutomaticKeepAliveClientMixin {
         // in case list is not full screen and remove ios Bouncing
         physics: AlwaysScrollableClampingScrollPhysics(),
         slivers: <Widget>[
-          // SliverPersistentHeader(
-          //   delegate: CommonSliverPersistentHeaderDelegate(
-          //     builder: (BuildContext context, double shrinkOffset,
-          //         bool overlapsContent) {
-          //     },
-          //     height: headerHeight,
+          _ChatListGroup(value: _ChatListGroupValue.ready),
+          _ChatListGroup(value: _ChatListGroupValue.cancel),
+          SliverToBoxAdapter(
+            child: SizedBox(height: 100),
+          ),
+          // SliverToBoxAdapter(
+          //   child: Material(
+          //     color: Colors.white,
+          //     child: InkWell(
+          //       child: Container(
+          //         decoration: BoxDecoration(
+          //           border: Border(
+          //             bottom: BorderSide(color: Theme.of(context).dividerColor),
+          //           ),
+          //         ),
+          //         padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          //         child: Row(
+          //           children: [
+          //             Container(
+          //               child: Text('Отменённые'),
+          //             ),
+          //             Spacer(),
+          //             Container(
+          //               padding: EdgeInsets.symmetric(horizontal: 2),
+          //               child: Row(
+          //                 children: [
+          //                   SizedBox(width: 4),
+          //                   Text('12'),
+          //                   Icon(
+          //                     _isExpand ? Icons.expand_less : Icons.expand_more,
+          //                     size: 20,
+          //                   )
+          //                 ],
+          //               ),
+          //               decoration: ShapeDecoration(
+          //                 color: Colors.grey.withOpacity(0.3),
+          //                 shape: StadiumBorder(),
+          //               ),
+          //             )
+          //           ],
+          //         ),
+          //       ),
+          //       onLongPress: () {}, // чтобы сократить время для splashColor
+          //       onTap: () {
+          //         setState(() {
+          //           _isExpand = !_isExpand;
+          //         });
+          //       },
+          //     ),
           //   ),
-          //   pinned: true,
           // ),
-          SliverToBoxAdapter(
-            child: Material(
-              color: Colors.white,
-              child: InkWell(
-                child: Container(
-                  decoration: BoxDecoration(
-                    border: Border(
-                      bottom: BorderSide(color: Theme.of(context).dividerColor),
-                    ),
-                  ),
-                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                  child: Row(
-                    children: [
-                      Container(
-                        child: Text('Отменённые'),
-                      ),
-                      Spacer(),
-                      Container(
-                        padding: EdgeInsets.symmetric(horizontal: 2),
-                        child: Row(
-                          children: [
-                            SizedBox(width: 4),
-                            Text('12'),
-                            Icon(
-                              _isExpand ? Icons.expand_less : Icons.expand_more,
-                              size: 20,
-                            )
-                          ],
-                        ),
-                        decoration: ShapeDecoration(
-                          color: Colors.grey.withOpacity(0.3),
-                          shape: StadiumBorder(),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                onLongPress: () {}, // чтобы сократить время для splashColor
-                onTap: () {
-                  setState(() {
-                    _isExpand = !_isExpand;
-                  });
-                },
-              ),
-            ),
-          ),
-          if (_isExpand)
-            SliverList(
-              // itemExtent: 50.0,
-              delegate: SliverChildBuilderDelegate(
-                (BuildContext context, int index) {
-                  return Container(
-                    height: 100,
-                    alignment: Alignment.center,
-                    color: Colors.lightBlue[100 * (index % 9)],
-                    child: Text('list item $index'),
-                  );
-                },
-                childCount: 20,
-              ),
-            ),
+          // if (_isExpand)
+          //   SliverList(
+          //     // itemExtent: 50.0,
+          //     delegate: SliverChildBuilderDelegate(
+          //       (BuildContext context, int index) {
+          //         return Container(
+          //           height: 100,
+          //           alignment: Alignment.center,
+          //           color: Colors.lightBlue[100 * (index % 9)],
+          //           child: Text('list item $index'),
+          //         );
+          //       },
+          //       childCount: 20,
+          //     ),
+          //   ),
           // SliverList(delegate: ,)
-          SliverToBoxAdapter(
-            child: Column(
-              children: <Widget>[
-                Container(height: 200, color: Colors.blue),
-                Container(height: 200, color: Colors.orangeAccent),
-                Container(height: 200, color: Colors.green),
-                Container(height: 200, color: Colors.pink),
-                Container(height: 200, color: Colors.yellow),
-                Container(height: 200, color: Colors.purple),
-                Container(height: 200, color: Colors.teal),
-              ],
-            ),
-          ),
-          SliverPersistentHeader(
-            delegate: CommonSliverPersistentHeaderDelegate(
-              builder: (BuildContext context, double shrinkOffset,
-                  bool overlapsContent) {
-                return Container(color: Colors.red);
-              },
-              height: 200,
-            ),
-            pinned: true,
-          ),
-          SliverToBoxAdapter(
-            child: Column(
-              children: <Widget>[
-                Container(height: 200, color: Colors.blue),
-                Container(height: 200, color: Colors.orangeAccent),
-                Container(height: 200, color: Colors.green),
-                Container(height: 200, color: Colors.pink),
-                Container(height: 200, color: Colors.yellow),
-                Container(height: 200, color: Colors.purple),
-                Container(height: 200, color: Colors.teal),
-              ],
-            ),
-          ),
-
           // LoadingMoreSliverList(
           //   SliverListConfig<NoticeItem>(
           //     sourceList: widget.sourceList,
@@ -255,6 +215,296 @@ class ChatState extends State<ChatList> with AutomaticKeepAliveClientMixin {
           // ),
         ],
       ),
+    );
+  }
+}
+
+enum _ChatListGroupValue { ready, cancel }
+
+class _ChatListGroup extends StatefulWidget {
+  const _ChatListGroup({
+    Key key,
+    this.value,
+  }) : super(key: key);
+
+  final _ChatListGroupValue value;
+
+  @override
+  _ChatListGroupState createState() => _ChatListGroupState();
+}
+
+class _ChatListGroupState extends State<_ChatListGroup>
+    with TickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+        duration: const Duration(milliseconds: 2000), vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Future<void> _playAnimation() async {
+    try {
+      await _controller.forward().orCancel;
+      await _controller.reverse().orCancel;
+    } on TickerCanceled {
+      // the animation got canceled, probably because we were disposed
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    timeDilation = 10.0; // 1.0 is normal animation speed.
+    final isExpanded = appState['${widget.value}'] == true;
+    return SliverToBoxAdapter(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          // AnimatedOpacity(
+          //   opacity: isExpanded ? 1 : 0,
+          //   duration: _kDuration,
+          //   child:
+          // ),
+          // Container(
+          //   child: SliverList(
+          //     // itemExtent: 50.0,
+          //     delegate: SliverChildBuilderDelegate(
+          //       (BuildContext context, int index) {
+          //         return Container(
+          //           height: 100,
+          //           alignment: Alignment.center,
+          //           color: Colors.lightBlue[100 * (index % 9)],
+          //           child: Text('list item $index'),
+          //         );
+          //       },
+          //       childCount: 20,
+          //     ),
+          //   ),
+          // ),
+          GestureDetector(
+            behavior: HitTestBehavior.opaque,
+            onTap: () {
+              _playAnimation();
+            },
+            child: Center(
+              child: Container(
+                width: 300.0,
+                height: 300.0,
+                decoration: BoxDecoration(
+                  color: Colors.black.withOpacity(0.1),
+                  border: Border.all(
+                    color: Colors.black.withOpacity(0.5),
+                  ),
+                ),
+                child: StaggerAnimation(controller: _controller.view),
+              ),
+            ),
+          ),
+          Material(
+            color: Colors.white,
+            child: InkWell(
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(color: Theme.of(context).dividerColor),
+                  ),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                child: Row(
+                  children: [
+                    Container(
+                      child: Text({
+                        _ChatListGroupValue.ready: 'Договоритесь о встрече',
+                        _ChatListGroupValue.cancel: 'Отменённые',
+                      }[widget.value]),
+                    ),
+                    Spacer(),
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 2),
+                      child: Row(
+                        children: [
+                          SizedBox(width: 4),
+                          Text('12'),
+                          Icon(
+                            isExpanded ? Icons.expand_less : Icons.expand_more,
+                            size: 20,
+                          )
+                        ],
+                      ),
+                      decoration: ShapeDecoration(
+                        color: Colors.grey.withOpacity(0.3),
+                        shape: StadiumBorder(),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              onLongPress: () {}, // чтобы сократить время для splashColor
+              onTap: () {
+                appState['${widget.value}'] = !isExpanded;
+                setState(() {});
+              },
+            ),
+          ),
+          if (isExpanded)
+            ListBox(
+              itemCount: 10,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  height: 100,
+                  alignment: Alignment.center,
+                  child: Text('list item $index'),
+                  decoration: BoxDecoration(
+                    color: Colors.lightBlue[100 * (index % 9)],
+                    border: Border(
+                      bottom: BorderSide(color: Theme.of(context).dividerColor),
+                    ),
+                  ),
+                );
+              },
+              separatorBuilder: (BuildContext context, int index) {
+                return SizedBox.shrink();
+              },
+            ),
+        ],
+      ),
+    );
+  }
+}
+
+class StaggerAnimation extends StatelessWidget {
+  StaggerAnimation({Key key, this.controller})
+      :
+
+        // Each animation defined here transforms its value during the subset
+        // of the controller's duration defined by the animation's interval.
+        // For example the opacity animation transforms its value during
+        // the first 10% of the controller's duration.
+
+        opacity = Tween<double>(
+          begin: 0.0,
+          end: 1.0,
+        ).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: Interval(
+              0.0,
+              0.100,
+              curve: Curves.ease,
+            ),
+          ),
+        ),
+        width = Tween<double>(
+          begin: 50.0,
+          end: 150.0,
+        ).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: Interval(
+              0.125,
+              0.250,
+              curve: Curves.ease,
+            ),
+          ),
+        ),
+        height = Tween<double>(begin: 50.0, end: 150.0).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: Interval(
+              0.250,
+              0.375,
+              curve: Curves.ease,
+            ),
+          ),
+        ),
+        padding = EdgeInsetsTween(
+          begin: const EdgeInsets.only(bottom: 16.0),
+          end: const EdgeInsets.only(bottom: 75.0),
+        ).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: Interval(
+              0.250,
+              0.375,
+              curve: Curves.ease,
+            ),
+          ),
+        ),
+        borderRadius = BorderRadiusTween(
+          begin: BorderRadius.circular(4.0),
+          end: BorderRadius.circular(75.0),
+        ).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: Interval(
+              0.375,
+              0.500,
+              curve: Curves.ease,
+            ),
+          ),
+        ),
+        color = ColorTween(
+          begin: Colors.indigo[100],
+          end: Colors.orange[400],
+        ).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: Interval(
+              0.500,
+              0.750,
+              curve: Curves.ease,
+            ),
+          ),
+        ),
+        super(key: key);
+
+  final Animation<double> controller;
+  final Animation<double> opacity;
+  final Animation<double> width;
+  final Animation<double> height;
+  final Animation<EdgeInsets> padding;
+  final Animation<BorderRadius> borderRadius;
+  final Animation<Color> color;
+
+  // This function is called each time the controller "ticks" a new frame.
+  // When it runs, all of the animation's values will have been
+  // updated to reflect the controller's current value.
+  Widget _buildAnimation(BuildContext context, Widget child) {
+    return Container(
+      padding: padding.value,
+      alignment: Alignment.bottomCenter,
+      child: Opacity(
+        opacity: opacity.value,
+        child: Container(
+          width: width.value,
+          height: height.value,
+          decoration: BoxDecoration(
+            color: color.value,
+            border: Border.all(
+              color: Colors.indigo[300],
+              width: 3.0,
+            ),
+            borderRadius: borderRadius.value,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      builder: _buildAnimation,
+      animation: controller,
     );
   }
 }
