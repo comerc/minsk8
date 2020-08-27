0b294a64edc69ba5e74d4dc6cae6d9b60bf3b092
comerc <andrew.kachanov@gmail.com>
2020-08-27 14:31:56 +0300

    Release _AnimatedBox

--- lib/widgets/chat_list.dart
+++ lib/widgets/chat_list.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart' show timeDilation;
-import 'package:intl/intl.dart';
 import 'package:extended_image/extended_image.dart';
 import 'package:loading_more_list/loading_more_list.dart';
 import 'package:pull_to_refresh_notification/pull_to_refresh_notification.dart';
@@ -25,8 +24,6 @@ class ChatList extends StatefulWidget {
 }
 
 class ChatState extends State<ChatList> with AutomaticKeepAliveClientMixin {
-  var _isExpand = true;
-
   @override
   bool get wantKeepAlive => true;
 
@@ -240,9 +237,8 @@ class _ChatListGroupState extends State<_ChatListGroup>
   @override
   void initState() {
     super.initState();
-
     _controller = AnimationController(
-        duration: const Duration(milliseconds: 2000), vsync: this);
+        duration: const Duration(milliseconds: 400), vsync: this);
   }
 
   @override
@@ -251,29 +247,24 @@ class _ChatListGroupState extends State<_ChatListGroup>
     super.dispose();
   }
 
-  Future<void> _playAnimation() async {
-    try {
-      await _controller.forward().orCancel;
-      await _controller.reverse().orCancel;
-    } on TickerCanceled {
-      // the animation got canceled, probably because we were disposed
-    }
-  }
+  // Future<void> _playAnimation() async {
+  //   try {
+  //     await _controller.forward().orCancel;
+  //     await _controller.reverse().orCancel;
+  //   } on TickerCanceled {
+  //     // the animation got canceled, probably because we were disposed
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
-    timeDilation = 10.0; // 1.0 is normal animation speed.
+    // timeDilation = 10.0; // 1.0 is normal animation speed.
     final isExpanded = appState['${widget.value}'] == true;
     return SliverToBoxAdapter(
       child: Column(
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          // AnimatedOpacity(
-          //   opacity: isExpanded ? 1 : 0,
-          //   duration: _kDuration,
-          //   child:
-          // ),
           // Container(
           //   child: SliverList(
           //     // itemExtent: 50.0,
@@ -290,25 +281,25 @@ class _ChatListGroupState extends State<_ChatListGroup>
           //     ),
           //   ),
           // ),
-          GestureDetector(
-            behavior: HitTestBehavior.opaque,
-            onTap: () {
-              _playAnimation();
-            },
-            child: Center(
-              child: Container(
-                width: 300.0,
-                height: 300.0,
-                decoration: BoxDecoration(
-                  color: Colors.black.withOpacity(0.1),
-                  border: Border.all(
-                    color: Colors.black.withOpacity(0.5),
-                  ),
-                ),
-                child: StaggerAnimation(controller: _controller.view),
-              ),
-            ),
-          ),
+          // GestureDetector(
+          //   behavior: HitTestBehavior.opaque,
+          //   onTap: () {
+          //     _playAnimation();
+          //   },
+          //   child: Center(
+          //     child: Container(
+          //       width: 300.0,
+          //       height: 300.0,
+          //       decoration: BoxDecoration(
+          //         color: Colors.black.withOpacity(0.1),
+          //         border: Border.all(
+          //           color: Colors.black.withOpacity(0.5),
+          //         ),
+          //       ),
+          //       child: StaggerAnimation(controller: _controller.view),
+          //     ),
+          //   ),
+          // ),
           Material(
             color: Colors.white,
             child: InkWell(
@@ -334,10 +325,17 @@ class _ChatListGroupState extends State<_ChatListGroup>
                         children: [
                           SizedBox(width: 4),
                           Text('12'),
-                          Icon(
-                            isExpanded ? Icons.expand_less : Icons.expand_more,
-                            size: 20,
-                          )
+                          _AnimatedIcon(
+                            controller: _controller.view,
+                            child: Icon(
+                              Icons.expand_more,
+                              size: 20,
+                            ),
+                          ),
+                          // Icon(
+                          //   isExpanded ? Icons.expand_less : Icons.expand_more,
+                          //   size: 20,
+                          // )
                         ],
                       ),
                       decoration: ShapeDecoration(
@@ -351,12 +349,18 @@ class _ChatListGroupState extends State<_ChatListGroup>
               onLongPress: () {}, // чтобы сократить время для splashColor
               onTap: () {
                 appState['${widget.value}'] = !isExpanded;
+                if (isExpanded) {
+                  _controller.reverse();
+                } else {
+                  _controller.forward();
+                }
                 setState(() {});
               },
             ),
           ),
-          if (isExpanded)
-            ListBox(
+          _AnimatedBox(
+            controller: _controller.view,
+            child: ListBox(
               itemCount: 10,
               itemBuilder: (BuildContext context, int index) {
                 return Container(
@@ -375,22 +379,56 @@ class _ChatListGroupState extends State<_ChatListGroup>
                 return SizedBox.shrink();
               },
             ),
+          ),
         ],
       ),
     );
   }
 }
 
-class StaggerAnimation extends StatelessWidget {
-  StaggerAnimation({Key key, this.controller})
-      :
+class _AnimatedIcon extends StatelessWidget {
+  _AnimatedIcon({
+    Key key,
+    Animation<double> controller,
+    this.child,
+  })  : turns = Tween<double>(
+          begin: 0.0,
+          end: -.5,
+        ).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: Interval(
+              0.0,
+              0.5,
+              curve: Curves.ease,
+            ),
+          ),
+        ),
+        super(key: key);
 
+  final Animation<double> turns;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return RotationTransition(
+      turns: turns,
+      child: child,
+    );
+  }
+}
+
+class _AnimatedBox extends StatelessWidget {
+  _AnimatedBox({
+    Key key,
+    this.controller,
+    this.child,
+  })  :
         // Each animation defined here transforms its value during the subset
         // of the controller's duration defined by the animation's interval.
         // For example the opacity animation transforms its value during
         // the first 10% of the controller's duration.
-
-        opacity = Tween<double>(
+        heightFactor = Tween<double>(
           begin: 0.0,
           end: 1.0,
         ).animate(
@@ -398,69 +436,20 @@ class StaggerAnimation extends StatelessWidget {
             parent: controller,
             curve: Interval(
               0.0,
-              0.100,
+              0.5,
               curve: Curves.ease,
             ),
           ),
         ),
-        width = Tween<double>(
-          begin: 50.0,
-          end: 150.0,
+        opacity = Tween<double>(
+          begin: 0.0,
+          end: 1.0,
         ).animate(
           CurvedAnimation(
             parent: controller,
             curve: Interval(
-              0.125,
-              0.250,
-              curve: Curves.ease,
-            ),
-          ),
-        ),
-        height = Tween<double>(begin: 50.0, end: 150.0).animate(
-          CurvedAnimation(
-            parent: controller,
-            curve: Interval(
-              0.250,
-              0.375,
-              curve: Curves.ease,
-            ),
-          ),
-        ),
-        padding = EdgeInsetsTween(
-          begin: const EdgeInsets.only(bottom: 16.0),
-          end: const EdgeInsets.only(bottom: 75.0),
-        ).animate(
-          CurvedAnimation(
-            parent: controller,
-            curve: Interval(
-              0.250,
-              0.375,
-              curve: Curves.ease,
-            ),
-          ),
-        ),
-        borderRadius = BorderRadiusTween(
-          begin: BorderRadius.circular(4.0),
-          end: BorderRadius.circular(75.0),
-        ).animate(
-          CurvedAnimation(
-            parent: controller,
-            curve: Interval(
-              0.375,
-              0.500,
-              curve: Curves.ease,
-            ),
-          ),
-        ),
-        color = ColorTween(
-          begin: Colors.indigo[100],
-          end: Colors.orange[400],
-        ).animate(
-          CurvedAnimation(
-            parent: controller,
-            curve: Interval(
-              0.500,
-              0.750,
+              0.5,
+              1.0,
               curve: Curves.ease,
             ),
           ),
@@ -469,42 +458,31 @@ class StaggerAnimation extends StatelessWidget {
 
   final Animation<double> controller;
   final Animation<double> opacity;
-  final Animation<double> width;
-  final Animation<double> height;
-  final Animation<EdgeInsets> padding;
-  final Animation<BorderRadius> borderRadius;
-  final Animation<Color> color;
-
-  // This function is called each time the controller "ticks" a new frame.
-  // When it runs, all of the animation's values will have been
-  // updated to reflect the controller's current value.
-  Widget _buildAnimation(BuildContext context, Widget child) {
-    return Container(
-      padding: padding.value,
-      alignment: Alignment.bottomCenter,
-      child: Opacity(
-        opacity: opacity.value,
-        child: Container(
-          width: width.value,
-          height: height.value,
-          decoration: BoxDecoration(
-            color: color.value,
-            border: Border.all(
-              color: Colors.indigo[300],
-              width: 3.0,
-            ),
-            borderRadius: borderRadius.value,
-          ),
-        ),
-      ),
-    );
-  }
+  final Animation<double> heightFactor;
+  final Widget child;
 
   @override
   Widget build(BuildContext context) {
     return AnimatedBuilder(
       builder: _buildAnimation,
       animation: controller,
+      child: child,
+    );
+  }
+
+  // This function is called each time the controller "ticks" a new frame.
+  // When it runs, all of the animation's values will have been
+  // updated to reflect the controller's current value.
+  Widget _buildAnimation(BuildContext context, Widget child) {
+    return Opacity(
+      opacity: opacity.value,
+      child: ClipRect(
+        child: Align(
+          alignment: Alignment.topCenter,
+          heightFactor: heightFactor.value < 1 ? 0 : 1, // hack for visible
+          child: child,
+        ),
+      ),
     );
   }
 }
