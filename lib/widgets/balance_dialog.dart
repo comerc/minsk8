fb08161cd2f1b1988be1b7a1d62a34413cba00b6
comerc <andrew.kachanov@gmail.com>
2020-10-05 12:04:44 +0300

    Delete legacy

--- lib/widgets/balance_dialog.dart
+++
@@ -1,3 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:minsk8/import.dart';
--- lib/widgets/notice_list.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-import 'package:extended_image/extended_image.dart';
-import 'package:loading_more_list/loading_more_list.dart';
-import 'package:pull_to_refresh_notification/pull_to_refresh_notification.dart';
-import 'package:extended_nested_scroll_view/extended_nested_scroll_view.dart'
-    as extended;
-import 'package:minsk8/import.dart';
