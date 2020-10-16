b3d63c25d4513e030ea048d6303fa0e95816738c
comerc <andrew.kachanov@gmail.com>
2020-10-16 05:31:27 +0300

    Comment legacy code

--- lib/widgets/wrapper.dart
+++ lib/widgets/wrapper.dart
@@ -20,12 +20,13 @@ class Wrapper extends StatefulWidget {
     this.pullToRefreshNotificationKey,
     this.poolForReloadTabs,
     this.hasAppBar = false,
-  })  : assert(
-          !dataPool.any(
-            (element) =>
-                !(element is SourceList || element is List<SourceList>),
-          ),
-        ),
+  }) :
+        // assert(
+        //   !dataPool.any(
+        //     (element) =>
+        //         !(element is SourceList || element is List<SourceList>),
+        //   ),
+        // ),
         super(key: key);
 
   final int tabIndex;
